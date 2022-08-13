81ca7da5721ed143402d4a0db77c5934bc2a855c
Andrei Micu <andrei.micu394@gmail.com>
2022-08-13 21:51:58 +0300

    Setup basic scaffolding of the "Fresh Socks" app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+        <div className="category-container">
+            {/*<img />*/}
+            <div className="category-body-container">
+                <h2>Books</h2>
+                <p>Shop now</p>
+            </div>
+        </div>
+        <div className="category-container">
+            {/*<img />*/}
+            <div className="category-body-container">
+                <h2>eBooks</h2>
+                <p>Shop now</p>
+            </div>
+        </div>
+        <div className="category-container">
+            {/*<img />*/}
+            <div className="category-body-container">
+                <h2>Accessories</h2>
+                <p>Shop now</p>
+            </div>
+        </div>
     </div>
   );
 }
