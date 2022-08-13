0a38e99ed64897596b014bb23627e76c6c89b7c5
Andrei Micu <andrei.micu394@gmail.com>
2022-08-13 23:32:10 +0300

    Add SasS, images and category menu + category item components.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
+import CategoryMenu from "./components/category-menu/category-menu.component";
+
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Books",
+      imageUrl:
+        "https://www.ukrgate.com/eng/wp-content/uploads/2021/02/The-Ukrainian-Book-Institute-Purchases-380.9-Thousand-Books-for-Public-Libraries1.jpeg",
+    },
+    {
+      id: 2,
+      title: "eBooks",
+      imageUrl:
+        "https://bogdanpopescu.com/wp-content/uploads/2020/07/free-ebooks.jpeg",
+    },
+    {
+      id: 3,
+      title: "Accessories",
+      imageUrl:
+        "https://m.media-amazon.com/images/I/81pLy81nyiL._AC_SL1500_.jpg",
+    },
+  ];
 
-    const categories = [
-        {
-            id: 1,
-            title: 'Books',
-        },
-        {
-            id: 2,
-            title: 'eBooks',
-        },
-        {
-            id: 3,
-            title: 'Accessories',
-        }
-    ]
-
-  return (
-    <div className='categories-container'>
-        {
-            categories.map(({title}) => (
-                <div className='category-container'>
-                    <div className='background-image' />
-                    <div className='category-body-container'>
-                        <h2>{title}</h2>
-                        <p>Shop now</p>
-                    </div>
-                </div>
-            ))
-        }
-    </div>
-  );
-}
+  return <CategoryMenu categories={categories} />;
+};
 
 export default App;
