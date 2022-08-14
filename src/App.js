f79df0d0bacebb27b7b02bb3932baff394015d69
Andrei Micu <andrei.micu394@gmail.com>
2022-08-14 15:20:20 +0300

    Add Home component and route to it, from App component.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import CategoryMenu from "./components/category-menu/category-menu.component";
+import { Route, Routes } from "react-router-dom";
+import Home from "./routes/home/home.component";
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Books",
-      imageUrl:
-        "https://www.ukrgate.com/eng/wp-content/uploads/2021/02/The-Ukrainian-Book-Institute-Purchases-380.9-Thousand-Books-for-Public-Libraries1.jpeg",
-    },
-    {
-      id: 2,
-      title: "eBooks",
-      imageUrl:
-        "https://bogdanpopescu.com/wp-content/uploads/2020/07/free-ebooks.jpeg",
-    },
-    {
-      id: 3,
-      title: "Accessories",
-      imageUrl:
-        "https://m.media-amazon.com/images/I/81pLy81nyiL._AC_SL1500_.jpg",
-    },
-  ];
-
-  return <CategoryMenu categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,28 @@
+import CategoryMenu from "./../../components/category-menu/category-menu.component";
+
+const Home = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Books",
+      imageUrl:
+        "https://www.ukrgate.com/eng/wp-content/uploads/2021/02/The-Ukrainian-Book-Institute-Purchases-380.9-Thousand-Books-for-Public-Libraries1.jpeg",
+    },
+    {
+      id: 2,
+      title: "eBooks",
+      imageUrl:
+        "https://bogdanpopescu.com/wp-content/uploads/2020/07/free-ebooks.jpeg",
+    },
+    {
+      id: 3,
+      title: "Accessories",
+      imageUrl:
+        "https://m.media-amazon.com/images/I/81pLy81nyiL._AC_SL1500_.jpg",
+    },
+  ];
+
+  return <CategoryMenu categories={categories} />;
+};
+
+export default Home;
